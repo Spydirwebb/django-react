c76d99c26bf1ac3af6fffd8459f18af970fbee1a
Spydirwebb <webb.jeffery.89@gmail.com>
2021-09-29 19:38:11 +0000

    join room working

--- music_controller/frontend/src/app.js
+++ music_controller/frontend/src/app.js
@@ -4,7 +4,7 @@ import HomePage from './components/HomePage';
 
 const App = () =>{
     return (
-        <div>
+        <div className="center">
             <HomePage />
         </div>
         
--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { TextField, Button, Grid, Typography } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
+const RoomJoinPage = (props) => {
+    const [roomCode, setRoomCode] = useState();
+    const [error, setError] = useState("")
 
-const RoomJoinPage = () => {
-    
+    const handleRoomCodeChange = (e) => {
+        setRoomCode(e.target.value)
+    }
+
+    const handleEnterRoomButtonPressed = () => {
+        console.log(roomCode)
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              code: roomCode,
+            }),
+          };
+        fetch('/api/join-room', requestOptions).then((response) => {
+            if(response.ok){
+                props.history.push(`/room/${roomCode}`)
+            }else{
+                setError("Room Not Found")
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
     return (
         <div>
-            <p>This is the join room page</p>
+            <Grid container spacing={1} alignITems="center">
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4" component="h4">
+                        Join a Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <TextField
+                        error={error}
+                        label="Code"
+                        placeholder="Enter a Room Code"
+                        value={roomCode}
+                        onChange={handleRoomCodeChange}
+                        helperText={error}
+                        variant="outlined" />
+                </Grid>
+                <Grid item xs={12} align="center">
+                <Button variant="contained" color="primary" component ={Link} onClick={handleEnterRoomButtonPressed}>
+                        Enter Room
+                    </Button>
+                </Grid>  
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="secondary" to="/" component ={Link}>
+                        Back
+                    </Button>
+                </Grid>
+            </Grid>
         </div>
     )
 }
